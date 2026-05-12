934340be5685a398688ce28f259fb736ef7560a4
lisboarafael644-spec <lisboarafael644@gmail.com>
2026-05-12 17:39:28 -0300

    atualizacao

---
+++ exercicio01.py
@@ -0,0 +1,24 @@
+n1 = input("digite o 1 numero:")
+n2 = input("digite o 2 numero:")
+resultado = n1 + n2
+print(int(n1))
+print(int(n2))
+resultado = n1 // n2
+resultado2 = n1 % n2
+resultado3 = n1 ** n2
+print("resultado e:",resultado)
+print("resultado e:",resultado2)
+print("resultado e:",resultado3)
+
+print("--------------------------------")
+print("    Operador Relacionais        ")
+print("--------------------------------")
+
+relacao1 = n1 >  n2
+relacao2 = n1 <  n2
+relacao3 = n1 >= n2
+relacao4 = n1 <= n2
+relacao5 = n1 == n2
+relacao6 = n1 != n2
+
+print(" OS resultados das relacao estarao abaixo: \n{}\n{}\n{}\n{}\n{}\n{}".format(relacao1, relacao2, relacao3, relacao4, relacao5, relacao6))
---
+++ programa01.py
@@ -0,0 +1,16 @@
+print("eai bigode")
+print("A historia começou com um relogio esquisito")
+print("1") #str (String de carcteres)
+print (1) #int (numero inteiro)
+print (-2) #int (numero inteiro)
+print (8.2) #float (Numero real ou decimal, ponto flutuante)
+print (True) #Verdadeiro (booleano - bool)
+print (False) #falso (booleano - bool)
+
+nome = "alucarde"
+idade = "590"
+peso = "95kg"
+maioridade = "true"
+
+print("esse e o",nome," ele tem",idade,"anos e pesa ",peso,"kg")
+print("hoje a aniversario da {} e ela completa {} anos e pesa {} alem disso ela e maior de idade: {}".format(nome,idade,peso,maioridade))
