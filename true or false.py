f947394797a2733680e6d0354fdefaf3f6c4eaac
lisboarafael644-spec <lisboarafael644@gmail.com>
2026-05-20 17:34:08 -0300

    exercicios condicionais

---
+++ true or false.py
@@ -0,0 +1,36 @@
+exercicio1 = (15>7) and (3==3)
+print(f"execicio1: {exercicio1}")
+
+
+ex2 = (10<4) or (8 != 5)
+print(f"execicio2: {ex2}")
+
+
+ex3 = not(20>=20)
+print(f"exercicio3: {ex3}")
+
+
+ex4 = (5*2==10)and not(3>4)
+print(f"exercicio4: {ex4}")
+
+
+ex5 = (12/2==5) or (7<=7)
+print(f"exercicio5: {ex5}")
+
+
+ex6 = not(9 != 9) and (4 > 1)
+print(f"exercicio6: {ex6}")
+
+
+ex7 = (100 == 100) and ((5 > 10)) or (2<3)
+print(f"exercicio7: {ex7}")
+
+
+ex8 = not(18 < 20) and ((5 != 5))
+
+
+ex9 = (0 == 1) or not ((3 >= 2) and ((4 <= 4)))
+print(f"exercicio8: {ex9}")
+
+ex10 = ((10 > 5) and not (2 == 3)) or (8<4)
+print(f"exercicio10: {ex10}")
