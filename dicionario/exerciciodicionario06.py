0a0f9678d626697646d516a13176740687c47c5f
lisboarafael644-spec <lisboarafael644@gmail.com>
2026-06-16 17:44:25 -0300

    projeto final uc

--- dicionario/exerciciodicionario06.py
+++ dicionario/exerciciodicionario06.py
@@ -1,15 +1,26 @@
-from email.policy import default
-
 estoque = {"teclado":15, "mouse":22, "monitor":8}
-
-while True:
-    nome, quatidade = input("digite o nome do produto que voce deseja comprar e a quantidade separdos: ").split(",")
+selecao = {}
+print(estoque)
+atulaliza_estoque = False
+continuar = "s"
+while continuar == "s":
+    nome, quatidade = input("digite o nome do produto que voce deseja comprar e a quantidade separdos por virgular: ").split(",")
 
     retorno = estoque.get(nome , "produto nao encontrado")
 
     for chave, valor in estoque.items():
-        if nome == chave:
-            estoque[chave] -= quatidade
+        if nome.lower() == chave.lower():
+            if valor == 0:
+                print("estoque esgotado")
+                continue
+            if valor < int(quatidade):
+                print("estoque insuficiente")
+                continue
+            else:
+                estoque[chave] -= int(quatidade)
+                atulaliza_estoque = True
 
-    print("estoque atualizado")
-    for chave, valor in estoque
+        if atulaliza_estoque:
+            print("estoque atualizado")
+            for chave, valor in estoque.items():
+                print(f"{chave} : {valor}")
